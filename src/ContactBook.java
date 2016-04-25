c2fac0b4027db91191fb37cce369139df2370fd2
kmangame0 <kmangame0@gmail.com>
2016-04-25 16:02:46 -0400

    Wrong files the first time.

--- src/ContactBook.java
+++ src/ContactBook.java
@@ -1,46 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Scanner;
 import java.util.TreeSet;
 
-public class ContactBook {
+public class ContactBook{
 	private TreeSet<Contact> ContactIndex;
-	private final static String ls = System.getProperty("line.separator");
+	File CB = new File("Contacts.txt");
 	
 	public ContactBook(){
-		ContactIndex = new TreeSet<Contact>(new ChainContactComparator
-				(new CompareFirstName(), new CompareLastName(), 
-				 new CompareEmail(), new ComparePhoneNumber()));
+		ContactIndex = new TreeSet<Contact>(new ChainContactComparator(new CompareFirstName(), new CompareLastName(), new CompareEmail(), new ComparePhoneNumber()));
+		CB = new File("Contacts.txt");
 	}
 	
-	public void addContactToBook(Contact c) {
+	public void addContactToBook(Contact c) throws IOException{
 		ContactIndex.add(c);
+		
 	}
-	
-	public void removeContactFromBook(Contact c) {
+	public void removefromBookandFile(Contact c) throws IOException{
 		ContactIndex.remove(c);
+		
+		File temp = new File("TempTestFile.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(CB));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
+		
+		String cl;
+		
+		while((cl = reader.readLine()) != null){
+			if(cl.contains(c.FirstName)&& cl.contains(c.LastName) && cl.contains(c.Email)&& cl.contains(c.PhoneNumber)){
+				continue;
+			}
+			writer.write(cl);
+		}
+		writer.close();
+		boolean successful = temp.renameTo(CB);
+		System.out.println(successful);
+		
 	}
 	
 	public void WriteCBToFile() throws IOException{
-		File CB = new File("Contacts.txt");
+		
 		PrintWriter pw = new PrintWriter(CB);
-		
-		pw.write(this.toString());
-		
+		for(Contact c: ContactIndex){
+			pw.write(c.toString()+"\r\n");
+		}
 		pw.close();
 	}
 	
 	public String toString(){
-		StringBuilder retVal = new StringBuilder();
-		
+		String retVal = "";
 		for(Contact c : ContactIndex){
-			retVal.append(c.toString().trim() + ls);
+			retVal += c.toString() +"\n";
 		}
-		retVal.setLength(retVal.length()-1);
-		return retVal.toString();
+		return retVal;
 	}
-	
-	public Contact[] GetContacts() {
-		return ContactIndex.toArray(new Contact[0]);
+	public TreeSet<Contact> getIndex(){
+		return ContactIndex;
 	}
 }
--- src/UI.java
+++ src/UI.java
@@ -1,24 +1,28 @@
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.logging.Handler;
+import java.util.regex.Pattern;
+
+import javax.swing.GroupLayout.Alignment;
+
 import javafx.application.Application;
-import javafx.collections.FXCollections;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBase;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
@@ -33,26 +37,36 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-import javafx.util.Callback;
+import javafx.util.BuilderFactory;
 public class UI extends Application{
 	private static ContactBook cb;
-	private static VBox LeftVBox;
-	private static Pane RightPane;
-	
+	Contact selected;
+	Contact person;
+	StackPane MainPane;
+	GridPane CenterGridPane;
+	VBox LeftVBox;
+	HBox LeftPane;
+	StackPane RightPane;
+	HBox internalRightVBoxMenu;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		StackPane MainPane = new StackPane();
-		GridPane CenterGridPane = new GridPane();
-		LeftVBox = new VBox();
-		RightPane = new Pane();
-		HBox internalRightVBoxMenu = new HBox();
+		selected = new Contact(null,null,null,null);
 		
+		MainPane = new StackPane();
+		CenterGridPane = new GridPane();
+		LeftVBox = new VBox();
+		LeftPane = new HBox();
+		RightPane = new StackPane();
+		internalRightVBoxMenu = new HBox();
 		
 		CenterGridPane.setAlignment(Pos.CENTER);
 		LeftVBox.setAlignment(Pos.CENTER);
-		//RightVBox.setAlignment(Pos.CENTER);
+		RightPane.setAlignment(Pos.CENTER);
 		CenterGridPane.setHgap(25);
 		CenterGridPane.setVgap(10);
 		//CenterGridPane.scaleXProperty().bind(MainPane.layoutXProperty());
@@ -61,26 +75,28 @@ public class UI extends Application{
 	
 		
 		BackgroundFill bckfill = new BackgroundFill(Color.ANTIQUEWHITE, null, new Insets(5));
-		BackgroundFill AntiqueBackFill = new BackgroundFill(Color.LINEN, null, new Insets(0));
+		BackgroundFill AntiqueBackFill = new BackgroundFill(Color.ANTIQUEWHITE, null, new Insets(0));
 		Background AntiqueBackground = new Background(AntiqueBackFill);
 		Background Gridbackground = new Background(bckfill);
 		CenterGridPane.setBackground(Gridbackground);
 		MainPane.setBackground(AntiqueBackground);
+		LeftPane.setBackground(AntiqueBackground);
 		LeftVBox.setBackground(AntiqueBackground);
 		RightPane.setBackground(AntiqueBackground);
 		
 		
-		
-		
 		BorderStroke bdstk = new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, new BorderWidths(2));
 		Border bd = new Border(bdstk);
-		LeftVBox.setBorder(bd);
+		LeftPane.setBorder(bd);
+		LeftPane.setPadding(new Insets(15));
+		LeftVBox.setPadding(new Insets(15));
 		RightPane.setBorder(bd);
 		 
 		//Labels for the HBox and VBox
 		
-		Font titleFont = Font.font("Lato Semibold", FontWeight.EXTRA_BOLD, 30);
-		Label CurrentContacts = new Label("Current Contacts");
+		Font titleFont = Font.font("Lato Semibold", FontWeight.EXTRA_BOLD, 25);
+		Label CurrentContacts = new Label("Contacts");
+		CurrentContacts.setTextAlignment(TextAlignment.CENTER);
 		Label ContactInfo = new Label("Contact Information");
 		CurrentContacts.setUnderline(true);
 		ContactInfo.setUnderline(true);
@@ -94,12 +110,24 @@ public class UI extends Application{
 		
 		//Add Delete Buttons
 		HBox buttonHBox = new HBox();
-		
 		Button AddContact = new Button("Add Contact");
 		AddContact.setOnAction(new AddHandler());
 		
 		Button DeleteContact = new Button("Delete Contact");
-		DeleteContact.setOnAction(new DeleteHandler());
+		DeleteContact.setOnAction((event)->{
+			try {
+				if(selected.FirstName!=null && selected.LastName!=null){
+					cb.removefromBookandFile(selected);
+			}
+			else{
+				UserMessage("No Contact Selected", "To Delete a Contact, You Must First Select What Contact To Delete");
+			}
+				cb.removefromBookandFile(selected);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 		
 		Button EditContact = new Button("Edit Contact");
 		
@@ -112,60 +140,59 @@ public class UI extends Application{
 		//end add delete buttons
 		MainPane.getChildren().add(CenterGridPane);
 		
-		CenterGridPane.setGridLinesVisible(true);
-		
-		
+		//CenterGridPane.setGridLinesVisible(true);
 		CenterGridPane.add(CurrentContacts, 0, 0);
 		CenterGridPane.add(ContactInfo, 1, 0);
-		CenterGridPane.add(LeftVBox, 0, 1);
+		CenterGridPane.add(LeftPane, 0, 1);
 		CenterGridPane.add(RightPane, 1, 1);
 		CenterGridPane.add(buttonHBox, 1, 2);
 		
+		LeftPane.getChildren().add(LeftVBox);
 	
-	
-		//Textfeild for contact information
-		TextArea txf = new TextArea();
+		//Label for contact information
+		Label txf = new Label();
+		Font displayContactFont = new Font(20);
+		txf.setFont(displayContactFont);
+		txf.setWrapText(true);
+		txf.setTextAlignment(TextAlignment.CENTER);
 		
 		txf.textProperty();
-		txf.setEditable(false);
-		txf.setText("Contact information is here");
+//		txf.setEditable(false);
+		txf.setText("Contact Information Displayed here");
 		txf.setMinWidth(RightPane.getWidth());
 		txf.setMinHeight(RightPane.getHeight());
+	
+		//scroll bar for contact buttons
+		ScrollBar scrl = new ScrollBar();
+		scrl.setOrientation(Orientation.VERTICAL);
+		scrl.setMin(0);
+		scrl.setMax(200);
+		scrl.setValue(0);
+//		scrl.setPrefHeight(LeftVBox.getHeight());
+		scrl.setLayoutX(LeftPane.getWidth() -10);
+		LeftPane.getChildren().add(scrl);
+	
+		scrl.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+                    LeftVBox.setLayoutY(-new_val.doubleValue());
+            }
+        });
 		
-		ListView<Contact> listView = new ListView<Contact>();
-		listView.setItems(FXCollections.observableArrayList(cb.GetContacts()));
 		
-		listView.setCellFactory(new Callback<ListView<Contact>, ListCell<Contact>>() {
-
-			@Override
-			public ListCell<Contact> call(ListView<Contact> param) {
-			
-				ListCell<Contact> cell = new ListCell<Contact>() {
-					
-					@Override
-					protected void updateItem(Contact c, boolean bln) {
-                        super.updateItem(c, bln);
-                        if (c != null) {
-                            setText(c.getLastName() + ", " + c.getFirstName());
-                        }
-                    }
-				};
-				cell.setOnMouseClicked(new EventHandler<MouseEvent>() {
-
-					@Override
-					public void handle(MouseEvent event) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-				});
-				return cell;
-			}
-		});
-		
-		LeftVBox.getChildren().add(listView);
+		for(Contact c : cb.getIndex()){
+			Button ContactButton = new Button(c.FirstName +" "+c.LastName);
+			LeftVBox.getChildren().add(ContactButton);
+			ContactButton.setOnAction((event)->{
+				txf.setText(c.toString());
+				selected = c;
+			});
+		}
+		LeftVBox.setSpacing(5);
 		
 		MainPane.setPadding(new Insets(10,10,10,10));
+		
+		
 		
 		//internalRightVBoxMenu.getChildren().addAll(AddContact, DeleteContact);
 		internalRightVBoxMenu.setAlignment(Pos.BOTTOM_CENTER);
@@ -173,20 +200,20 @@ public class UI extends Application{
 		RightPane.getChildren().add(internalRightVBoxMenu);
 		
 		
-		
-		
-		Scene scene = new Scene(MainPane, 700, 500);	
+		Scene scene = new Scene(MainPane, 600, 400);	
 		primaryStage.setTitle("Contact Book");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
 	
+	
 	public class AddHandler implements EventHandler<ActionEvent>{
 		@Override
 		public void handle(ActionEvent event) {
 			try {
-				addPrompt();
+				Contact blank = new Contact(null,null,null,null);
+				alterPrompt(blank);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -197,48 +224,49 @@ public class UI extends Application{
 	public class EditHandler implements EventHandler<ActionEvent>{
 		@Override
 		public void handle(ActionEvent event) {
-			editContact();
-		}
-	}
-	
-	public class DeleteHandler implements EventHandler<ActionEvent>{
-		@Override
-		public void handle(ActionEvent e){
-			deleteContact();
-		}
-	}
-	
-	public static void main(String[] args) {
-		cb = new ContactBook();
-		
-		try {
-			FileInputStream fstream = new FileInputStream("Contacts.txt");
-			DataInputStream in = new DataInputStream(fstream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String s;
-			while((s = br.readLine()) != null) {
-				String[] tokens = s.split(",");
-				Contact c = new Contact(tokens[0], tokens[1], tokens[2], tokens[3]);
-				cb.addContactToBook(c);
+			try{
+				if(selected.FirstName!=null){
+					alterPrompt(selected);
+				}
+				else{
+					UserMessage("No Contact Selected", "To edit a Contact, You Must First Select What Contact To Edit");
+				}
+				
+			}catch (IOException e){
+				e.printStackTrace();
 			}
-			in.close();
-		}catch(Exception e) {
-			System.out.println(e.getStackTrace());
 		}
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		cb = new ContactBook();
+		File fi = new File("Contacts.txt");
+		
+		if(fi.exists()){
+			Scanner sc = new Scanner(fi);
+			sc.useDelimiter(Pattern.compile(",|(\r\n)"));
+			while(sc.hasNext()){
+				String f; String l; String e; String p;
+				f = sc.next();
+				l = sc.next();
+				e = sc.next();
+				p = sc.next();
+				Contact c = new Contact(f,l,e,p);
+				cb.addContactToBook(c);
+				System.out.println(c.toString());
+			}
+		}
+		else{
+			System.out.println("Creating new Contacts File");
+		}
+		
 		launch(args);
 	}
 
-	public void deleteContact(){
-		
-		
-		cb.removeContactFromBook(null);
-	}
 	
-	public void editContact(){
-		System.out.println("Editing a contact");
-	}
-	
-	public void addPrompt() throws IOException{
+	public void alterPrompt(Contact c) throws IOException{
+		
 		Stage popup = new Stage();
 		GridPane MainPane = new GridPane();
 		MainPane.setPadding(new Insets(10));
@@ -249,6 +277,14 @@ public class UI extends Application{
 		Label PhoneNumber = new Label("Phone Number");		TextField TXPHONENUMBER = new TextField();
 		
 		Button FinalizeAdd = new Button("Done");
+			if(c.FirstName!=null){
+			
+			TXFNAME.setText(c.FirstName);
+			TXLNAME.setText(c.LastName);
+			TXEMAIL.setText(c.Email);
+			TXPHONENUMBER.setText(c.PhoneNumber);
+			}
+		
 		
 		FinalizeAdd.setOnAction((event) -> {
 			String txfFirstName = TXFNAME.getText();
@@ -257,15 +293,23 @@ public class UI extends Application{
 			String txfPhoneNumber = TXPHONENUMBER.getText();
 			
 			try {
-				Contact c = new Contact(txfFirstName, txfLastName, txfEmail, txfPhoneNumber);
-				cb.addContactToBook(c);
+				Contact person = new Contact(txfFirstName, txfLastName, txfEmail, txfPhoneNumber);
+				cb.addContactToBook(person);
 				cb.WriteCBToFile();
 				System.out.println(cb.toString());
+				
+				
+				
+				UserMessage("Confirmation", person.FirstName +" "+person.LastName +" has been Added");
+				
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			popup.close();
+			
+			
 		});
 		
 		MainPane.add(FName, 0, 0);	MainPane.add(TXFNAME, 1, 0);
@@ -273,10 +317,23 @@ public class UI extends Application{
 		MainPane.add(Email, 0, 2);	MainPane.add(TXEMAIL, 1, 2);
 		MainPane.add(PhoneNumber, 0, 3);	MainPane.add(TXPHONENUMBER, 1, 3);
 		MainPane.add(FinalizeAdd, 1, 4);
+		
 
 		Scene scene = new Scene(MainPane, 280, 180);	
 		popup.setTitle("Add Contact");
 		popup.setScene(scene);
 		popup.show();
+	}
+	public void UserMessage(String Title, String message){
+		Stage s = new Stage();
+		StackPane p = new StackPane();
+		p.setAlignment(Pos.CENTER);
+		Label label = new Label(message);
+		label.setAlignment(Pos.CENTER);
+		p.getChildren().add(label);
+		Scene cscene = new Scene(p, 400,20);
+		s.setTitle(Title);
+		s.setScene(cscene);
+		s.show();
 	}
 }
